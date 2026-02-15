3694c7a676a933e2f40b7078a7ec3748b2b63420
KIMJEONGHYEON <hyeoniverse.dev@gmail.com>
2026-02-15 14:49:56 +0900

    fix: Lenis 무한 스크롤 기본값 복원
    infinite 옵션 기본값이 false로 변경되어 홈 페이지 무한 스크롤이 비활성화된 문제 수정

--- src/providers/LenisProvider.tsx
+++ src/providers/LenisProvider.tsx
@@ -77,7 +77,7 @@ export function LenisProvider({ children, options = {} }: LenisProviderProps) {
       smoothWheel: options.smoothWheel ?? true,
       wheelMultiplier: options.wheelMultiplier ?? 1,
       touchMultiplier: options.touchMultiplier ?? 2,
-      infinite: isMobile ? false : (options.infinite ?? false),
+      infinite: isMobile ? false : (options.infinite ?? true),
     });
 
     lenisRef.current = lenisInstance;
@@ -128,7 +128,7 @@ export function LenisProvider({ children, options = {} }: LenisProviderProps) {
     const handleResize = () => {
       const mobile = window.innerWidth <= 768;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (lenisInstance as any).options.infinite = mobile ? false : (options.infinite ?? false);
+      (lenisInstance as any).options.infinite = mobile ? false : (options.infinite ?? true);
     };
     window.addEventListener("resize", handleResize);
 
